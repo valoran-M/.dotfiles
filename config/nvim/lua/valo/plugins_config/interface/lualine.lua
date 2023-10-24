40eda98beb81e6612bfcc9110b9cbe001efe38d5
valomay <valomay@protonmail.com>
2023-10-24 12:04:28 +0200

    added %

--- config/nvim/lua/valo/plugins_config/interface/lualine.lua
+++ config/nvim/lua/valo/plugins_config/interface/lualine.lua
@@ -49,10 +49,8 @@ local location = {
 local progress = function()
 	local current_line = vim.fn.line(".")
 	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
 	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+	return string.format("%3.f", math.floor(line_ratio * 100)) .. "%%"
 end
 
 local spaces = function()
